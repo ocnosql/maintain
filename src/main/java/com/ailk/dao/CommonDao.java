db703d0395037aaa4ad30552f194800833f25d6d
hnwsha <hnwsha@126.com>
2016-08-15 15:40:04 +0800

    common 公共方法

---
+++ src/main/java/com/ailk/dao/CommonDao.java
@@ -0,0 +1,38 @@
+package com.ailk.dao;
+
+import com.ailk.oci.ocnosql.client.jdbc.HbaseJdbcHelper;
+import com.ailk.oci.ocnosql.client.jdbc.phoenix.PhoenixJdbcHelper;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wsh on 2016/8/11.
+ */
+public class CommonDao {
+
+    public List<Map<String, Object>> queryColumnsByTable(String schema, String table) throws SQLException{
+        String sql = "select COLUMN_NAME from system.catalog where table_name = '" + table +"' and column_family is not null";
+        if(schema != null && "DEFAULT".equals(schema)) {
+            sql = sql + " and table_schem is null";
+        } else {
+            sql = sql + " and table_schem = '" + schema +"'";
+        }
+        HbaseJdbcHelper help = new PhoenixJdbcHelper();
+        List<Map<String, Object>> results = help.executeQuery(sql);
+        return results;
+    }
+
+    public List<Map<String, Object>> queryAllColumnsByTable(String schema, String table) throws SQLException{
+        String sql = "select COLUMN_NAME from system.catalog where table_name = '" + table +"' ";
+        if(schema != null && "DEFAULT".equals(schema)) {
+            sql = sql + " and table_schem is null";
+        } else {
+            sql = sql + " and table_schem = '" + schema +"'";
+        }
+        HbaseJdbcHelper help = new PhoenixJdbcHelper();
+        List<Map<String, Object>> results = help.executeQuery(sql);
+        return results;
+    }
+}
---
+++ src/main/java/com/ailk/web/CommonAction.java
@@ -0,0 +1,99 @@
+package com.ailk.web;
+
+import com.ailk.core.base.action.BaseAction;
+import com.ailk.model.ResultDTO;
+import com.ailk.model.ValueSet;
+import com.ailk.model.ext.JsonResult;
+import com.ailk.model.ext.ResultBuild;
+import com.ailk.oci.ocnosql.client.jdbc.HbaseJdbcHelper;
+import com.ailk.oci.ocnosql.client.jdbc.phoenix.PhoenixJdbcHelper;
+import com.ailk.service.IQueryService;
+import com.ailk.service.impl.CommonService;
+import com.ailk.service.impl.DeleteMR;
+import com.ailk.service.impl.DeleteService;
+import com.google.gson.Gson;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts2.ServletActionContext;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wsh on 2016/7/13.
+ */
+
+public class CommonAction extends BaseAction  {
+    private static Log log = LogFactory.getLog(CommonAction.class);
+    private String tablespace;
+
+    public String getTableSpaces() {
+        String sql = "select distinct table_schem from system.catalog where table_schem <> 'SYSTEM' or table_schem is null";
+        HbaseJdbcHelper help = new PhoenixJdbcHelper();
+        Gson gs = new Gson();
+        List<Map<String, Object>> results = null;
+        try {
+             results = help.executeQuery(sql);
+            if(results != null && results.size() > 0) {
+                for(Map<String, Object> map : results) {
+                    if(map.get("TABLE_SCHEM") == null) {
+                        map.put("TABLE_SCHEM","DEFAULT");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error("查询异常", e);
+            this.setAjaxStr(gs.toJson(ResultBuild.buildFailed(e.getMessage())));
+        }
+        HashMap<String, Object> json = new HashMap<String, Object>();
+        json.put("root", results);
+        this.setAjaxStr(gs.toJson(json));
+        return AJAXRTN;
+    }
+
+    public String getTables() {
+        String sql = "select distinct table_name from system.catalog where table_schem";
+        String whereis = " = '" + tablespace + "'";
+        if(tablespace == null || "".equals(tablespace)) {
+            tablespace = "______________";
+        } else if("DEFAULT".equals(tablespace)) {
+            whereis = " is null";
+        }
+
+        String SQL = sql + whereis;
+        HbaseJdbcHelper help = new PhoenixJdbcHelper();
+        Gson gs = new Gson();
+        List<Map<String, Object>> results = null;
+        try {
+            results = help.executeQuery(SQL);
+        } catch (SQLException e) {
+            LOG.error("查询异常", e.getMessage());
+            this.setAjaxStr(gs.toJson(ResultBuild.buildFailed(e.getMessage())));
+        }
+        HashMap<String, Object> json = new HashMap<String, Object>();
+        json.put("root", results);
+        this.setAjaxStr(gs.toJson(json));
+        return AJAXRTN;
+    }
+
+    public String getColmns() throws SQLException {
+        ValueSet vs = new ValueSet();
+        bindParams(vs, ServletActionContext.getRequest());
+        CommonService commonService = new CommonService();
+        String schema = String.valueOf(vs.get("schema"));
+        String table = String.valueOf(vs.get("table"));
+        HashMap<String, Object> json = new HashMap<String, Object>();
+        json.put("root", commonService.getColumns(schema, table));
+        Gson gs = new Gson();
+        this.setAjaxStr(gs.toJson(json));
+        return AJAXRTN;
+    }
+
+    public void setTablespace(String tablespace) {
+        this.tablespace = tablespace;
+    }
+}
